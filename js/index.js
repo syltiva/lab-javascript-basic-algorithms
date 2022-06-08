bcf1cb5a27fa6a9e4fec7ff9030c33c16a4aed10
Sylvia Jung <92225958+syltiva@users.noreply.github.com>
2022-06-08 14:17:28 -0400

    bonuses

--- js/index.js
+++ js/index.js
@@ -25,3 +25,37 @@ console.log("The driver's name goes first.")
 } else {
   console.log("Yo, the navigator goes first definitely.")
 } 
+
+// Bonus 1
+const loremStr = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+const splitLorem = loremStr.split(" ")
+console.log(splitLorem)
+console.log(splitLorem.length)
+
+// Bonus 2
+// "A man, a plan, a canal, Panama!"
+// "Amor, Roma"
+// "race car"
+// "stack cats"
+// "step on no pets"
+// "taco cat"
+// "put it up"
+// "Was it a car or a cat I saw?" and "No 'x' in Nixon".
+
+const phraseToCheck = "Was it a car or a cat I saw?"
+
+function checkPalindrome(string) {
+  const editedStr = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")
+  console.log("editedStr", editedStr)
+  const reversedStr = editedStr.split("").reverse().join("")
+  console.log("reversedStr", reversedStr)
+  if (editedStr === reversedStr) {
+    console.log("Palindrome")
+  } else {
+    console.log("Not a Palindrome")
+  }
+    
+}
+
+checkPalindrome(phraseToCheck)
